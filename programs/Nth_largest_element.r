e537ccef042c0b81e3057af4193d842d927c97fd
Don Sabu <108197083+don-sabu@users.noreply.github.com>
2024-04-03 11:01:46 +0530

    Update Nth_largest_element.r

--- programs/Nth_largest_element.r
+++ programs/Nth_largest_element.r
@@ -3,7 +3,7 @@
 # Program: Nth largest element
 
 v <- sample(1:100, 10, replace=F) 
-print(v)
+cat("Vector: ", v, "\n")
 n = as.integer(readline(prompt ="Enter value of n: "))
 sorted_v <- sort(v, decreasing = TRUE)
 cat(n,"th largest element in the vector is: ",sorted_v[n])
