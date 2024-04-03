5aef42db7cf6187628a773b7d8ca0d2760113bde
Don Sabu <108197083+don-sabu@users.noreply.github.com>
2024-04-03 12:41:38 +0530

    Update maxmin.R

--- programs/maxmin.R
+++ programs/maxmin.R
@@ -1,3 +1,8 @@
+# Exp No: 02
+# Author: Don Sabu 21AD024
+# Program: Maximum and minimum number
+
 a = c(1,2,3,4,5)
+print(a)
 print(paste("Largest number: ",max(a)))
 print(paste("Smallest number: ",min(a)))
